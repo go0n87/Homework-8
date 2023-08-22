5bc90d99a7566b1c022d85d107b36f9dfcfdc2de
go0n87 <98361866+go0n87@users.noreply.github.com>
2023-08-22 20:49:36 +0300

    20:49 22.08.2023

--- Homework-8/Assets/MovingScript.cs
+++ Homework-8/Assets/MovingScript.cs
@@ -13,8 +13,7 @@ public class MovingScript : MonoBehaviour
     public Transform Point4;
     public Transform Point5;
     public Transform Point6;
-    public Transform Point7;
-    public Transform Point8;
+    public Transform Point7;    
 
     public GameObject Object1;
     public GameObject Object2;
@@ -22,20 +21,19 @@ public class MovingScript : MonoBehaviour
     public GameObject Object4;
     public GameObject Object5;
     public GameObject Object6;
-    public GameObject Object7;
+    //public GameObject Object7;
 
     [SerializeField]private Vector3 CurrentPoint;
     [SerializeField]private GameObject CurrObject;
 
-    [SerializeField] private int NumberOfPoint = 1;
+    [SerializeField] private int NumberOfPoint = 0;
     [SerializeField] private int NumberOfObject = 0;
 
-    [SerializeField] private bool ReverseObject = false;
-    [SerializeField] private bool ReversePoint = false;
+    [SerializeField] private bool Reverse = false;
 
     void Start()
     {
-        Points = new Vector3[8];
+        Points = new Vector3[7];
 
         Points[0] = Point1.position;
         Points[1] = Point2.position;
@@ -44,18 +42,17 @@ public class MovingScript : MonoBehaviour
         Points[4] = Point5.position;
         Points[5] = Point6.position;
         Points[6] = Point7.position;
-        Points[7] = Point8.position;
 
-        CurrentPoint = Points[1];
+        CurrentPoint = Points[0];
 
-        Objects = new GameObject[7];
+        Objects = new GameObject[6];
         Objects[0] = Object1;
         Objects[1] = Object2;
         Objects[2] = Object3;
         Objects[3] = Object4;
         Objects[4] = Object5;
         Objects[5] = Object6;
-        Objects[6] = Object7;
+        //Objects[6] = Object7;
 
         CurrObject = Objects[0];
 
@@ -75,34 +72,30 @@ public class MovingScript : MonoBehaviour
     private void ChangeCurrentPoint()
     {
 
-        if (NumberOfPoint == Points.Length-1 && NumberOfObject == 6)
+        if (NumberOfPoint == 6 && NumberOfObject == 5 || NumberOfPoint == 0 && NumberOfObject == 0)
         {
-            ReversePoint = !ReversePoint;
-        }
-        //else if (NumberOfPoint == 0)
-        //{
-         //   ReversePoint = !ReversePoint;
-        //}
+            Reverse = !Reverse;
+        }       
         else
         {
             ChangeCurrentObject();
         }
 
-        int ReverseNumber = ReversePoint == false ? 1 : -1;            
+        int ReverseNumber = Reverse == false ? 1 : -1;            
 
         CurrentPoint = Points[NumberOfPoint + ReverseNumber];
         
-        NumberOfPoint = += ReverseNumber;        
+        NumberOfPoint += ReverseNumber;        
     }
     private void ChangeCurrentObject()
     {
         //if (NumberOfObject == 0 || NumberOfObject == Objects.Length - 1)
-        if (NumberOfObject == Objects.Length && NumberOfPoint == 7)
-        {
-            ReverseObject = !ReverseObject;
-        }
+        //if (NumberOfObject == Objects.Length && NumberOfPoint == 8)
+        //{
+        //    ReverseObject = !ReverseObject;
+        //}
 
-        int ReverseNumber = ReverseObject == false ? 1 : -1;
+        int ReverseNumber = Reverse == false ? 1 : -1;
 
         CurrObject = Objects[NumberOfObject + ReverseNumber];
 
