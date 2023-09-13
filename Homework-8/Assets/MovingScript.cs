31a53d2856038417e06d95f5a74c9d32080d26b1
go0n87 <go0n@mail.ru>
2023-09-13 13:02:45 +0300

    13:02 13.09.2023

--- Homework-8/Assets/MovingScript.cs
+++ Homework-8/Assets/MovingScript.cs
@@ -5,26 +5,12 @@ using UnityEngine;
 
 public class MovingScript : MonoBehaviour
 { 
-
-    public Transform Point1;
-    public Transform Point2;
-    public Transform Point3;
-    public Transform Point4;
-    public Transform Point5;
-    public Transform Point6;
-    public Transform Point7;
-
     public static event Action<GameObject> FlagChanged;
 
-    private Vector3[] Points;
-    private GameObject[] Objects;
+    public Transform[] Points;
+    public GameObject[] Objects;
 
-    public GameObject Object1;
-    public GameObject Object2;
-    public GameObject Object3;
-    public GameObject Object4;
-    public GameObject Object5;
-    public GameObject Object6;
+    public float passDistance;
 
     private Vector3 CurrentPoint;
 
@@ -36,37 +22,20 @@ public class MovingScript : MonoBehaviour
 
     private bool Reverse = false;
 
+    
     void Start()
     {
-        Points = new Vector3[7];
 
-        Points[0] = Point1.position;
-        Points[1] = Point2.position;
-        Points[2] = Point3.position;
-        Points[3] = Point4.position;
-        Points[4] = Point5.position;
-        Points[5] = Point6.position;
-        Points[6] = Point7.position;
-
-        CurrentPoint = Points[0];
-
-        Objects = new GameObject[6];
-        Objects[0] = Object1;
-        Objects[1] = Object2;
-        Objects[2] = Object3;
-        Objects[3] = Object4;
-        Objects[4] = Object5;
-        Objects[5] = Object6;
-
+        CurrentPoint = Points[0].position;
         CurrObject = Objects[0];
-
-        CurrObject.transform.LookAt(CurrentPoint);
+        CurrObject.transform.LookAt(CurrentPoint);       
 
     }
 
     void Update()
     {
-        if (CurrentPoint == CurrObject.transform.position)
+
+        if (Vector3.Distance(CurrentPoint, CurrObject.transform.position) < passDistance)
         {
             ChangeCurrentPoint();
             CurrObject.transform.LookAt(CurrentPoint,Vector3.up);
@@ -77,8 +46,8 @@ public class MovingScript : MonoBehaviour
 
     private void ChangeCurrentPoint()
     {
-
-        if (NumberOfPoint == 6 && NumberOfObject == 5 || NumberOfPoint == 0 && NumberOfObject == 0)
+        
+        if (NumberOfPoint == Points.Length-1 && NumberOfObject == Objects.Length - 1 || NumberOfPoint == 0 && NumberOfObject == 0)
         {
             Reverse = !Reverse;
         }       
@@ -87,18 +56,9 @@ public class MovingScript : MonoBehaviour
             ChangeCurrentObject();
         }
 
-        int ReverseNumber = Reverse == false ? 1 : -1;
+        int ReverseNumber = Reverse == false ? 1 : -1;    
 
-        if (Reverse)
-        {
-            Points[NumberOfPoint] += new Vector3(0.5f, 0f, 0.5f);            
-        }
-        else            
-        {
-            Points[NumberOfPoint] += new Vector3(-0.5f, 0f, -0.5f);
-        }
-
-        CurrentPoint = Points[NumberOfPoint + ReverseNumber];
+        CurrentPoint = Points[NumberOfPoint + ReverseNumber].position;
         
         NumberOfPoint += ReverseNumber;        
     }
@@ -111,8 +71,6 @@ public class MovingScript : MonoBehaviour
         CurrObject = Objects[CurrentIndex];
 
         CurrentHandGrabber = GameObject.Find("GrabHand"+ CurrentIndex.ToString());
-
-        Debug.Log("GrabHand" + CurrentIndex.ToString());
 
         FlagChanged?.Invoke(CurrentHandGrabber);
 
